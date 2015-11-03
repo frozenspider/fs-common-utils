8ec7856e19fb44be202ea47d650ac9f45a96400b
Alex Abd <Alexander.Abdugafarov@nitka.com>
2015-11-04 01:02:16 +0500

    Added Implicits object

---
+++ src/main/scala/org/fs/utility/Implicits.scala
@@ -0,0 +1,72 @@
+package org.fs.utility
+
+import java.io.PrintWriter
+import java.io.StringWriter
+
+import scala.collection.GenIterableLike
+import scala.collection.GenTraversableLike
+import scala.collection.IndexedSeqLike
+import scala.collection.generic.CanBuildFrom
+
+object Implicits {
+  // Type aliases to make signatures shorter
+  private type GTL[+A, +Repr] = GenTraversableLike[A, Repr]
+  private type CBF[-From, -Elem, +To] = CanBuildFrom[From, Elem, To]
+
+  /** Iterable[X] enriched with some of most general support methods */
+  implicit class RichIterable[A, Repr](iter: GenIterableLike[A, Repr]) {
+
+    def mapWithIndex[B, Repr2 <: GTL[(A, Int), Repr2], That2](f: (A, Int) => B)(
+      implicit bf1: CBF[Repr, (A, Int), Repr2], bf2: CBF[Repr2, B, That2]): That2 =
+      {
+        iter.zipWithIndex map (x => f(x._1, x._2))
+      }
+
+    def eachWithIndex[U, That <: GTL[(A, Int), _]](f: (A, Int) => U)(
+      implicit bf1: CBF[Repr, (A, Int), That]): Unit =
+      {
+        iter.zipWithIndex foreach (x => f(x._1, x._2))
+      }
+
+    def toOption: Option[Repr] = {
+      if (iter.isEmpty) None
+      else Some(iter.repr)
+    }
+
+    def dropRightWhile(pred: A => Boolean)(implicit bf: CBF[Repr, A, Repr]): Repr = {
+      val content = iter.toSeq.reverse.dropWhile(pred).reverse
+      val builder = bf()
+      content.foreach(builder += _)
+      builder.result()
+    }
+  }
+
+  /** IndexedSeqLike[X] enriched with some of most general support methods */
+  implicit class RichIndexedSeqLike[A, Repr](is: IndexedSeqLike[A, Repr]) {
+    def get(i: Int): Option[A] = {
+      if (is.length <= i) None
+      else Some(is(i))
+    }
+
+    def getOrElse[B >: A](i: Int, default: => B): B =
+      get(i).getOrElse(default)
+  }
+
+  /** Traversable[Option[X]] enriched with some of most general support methods */
+  implicit class RichOptionsTraversable[A, Repr <: GTL[Option[A], Repr]](iter: GTL[Option[A], Repr]) {
+    /** @return collection of defined values */
+    def yieldDefined[Repr2 <: GTL[A, Repr2]](implicit bf: CBF[Repr, A, Repr2]): Repr2 = {
+      iter filter (_.isDefined) map (_.get)
+    }
+  }
+
+  /** Throwable enriched with some of most general support methods */
+  implicit class RichThrowable[Th <: Throwable](th: Th) {
+    /** @return stack trace printed to a string */
+    def stackTraceString: String = {
+      val writer = new StringWriter
+      th.printStackTrace(new PrintWriter(writer, true))
+      writer.toString
+    }
+  }
+}
